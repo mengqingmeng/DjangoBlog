1829feeee024962d0c8bb08bdf2379a42775a398
mengqingmeng <762577099@qq.com>
2016-10-29 16:40:29 +0800

    “first edit fork django blog”

--- weblog/config/settings.py
+++ weblog/config/settings.py
@@ -93,10 +93,20 @@ WSGI_APPLICATION = 'weblog.config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'database/db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'python',
+        'USER': 'pydev',
+        'PASSWORD': 'Meng@1992',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
